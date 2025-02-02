116947eb573f7fc7421f4e55f4d4d7e5ef18fab2
dheredia91 <113152681+Dheredia91@users.noreply.github.com>
2025-02-01 20:32:33 -0800

    reset

--- my-app/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    trailingSlash: true, // Ensures Netlify serves static files correctly
-  };
-  
-  export default nextConfig;
-  
