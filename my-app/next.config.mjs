4ec8bd9677ab98098715e1e3594901e9c75a7114
dheredia91 <113152681+Dheredia91@users.noreply.github.com>
2025-02-01 19:55:42 -0800

    fix config

---
+++ my-app/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    trailingSlash: true, // Ensures Netlify serves static files correctly
+  };
+  
+  export default nextConfig;
+  
